7c245db0fedb2a4077104dcf9a7a6842d3fd72b5
questbeat <questbeat@gmail.com>
2020-06-08 00:06:57 +0900

    Fix error on adding multiple files

--- functions/__git_add.fish
+++ functions/__git_add.fish
@@ -1,10 +1,7 @@
 function __git_add
   commandline | read -l buffer
-  git status --porcelain | \
-        angler "$ANGLER_QUERY_OPTION '$buffer'" | \
-        awk -v git_root=:/ '{print git_root$2}' | \
-        tr '\n' ' ' | \
-        read -l selected_files
+  set selected_files (git status --porcelain | angler "$ANGLER_QUERY_OPTION '$buffer'" | \
+    cut -d ' ' -f 2- | sed -e 's/^/:\//' | string escape -n)
   if test -n "$selected_files"
     commandline "git add $selected_files"
     commandline -f execute
