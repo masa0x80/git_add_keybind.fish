8fffe97116a19a869f50956ff971502c180881bf
KIMURA Masayuki <masa0x80@gmail.com>
2020-06-09 19:59:13 +0900

    Bug fix

--- functions/__git_add.fish
+++ functions/__git_add.fish
@@ -1,10 +1,10 @@
 function __git_add
-  commandline | read -l buffer
-  set selected_files (git status --porcelain | angler "$ANGLER_QUERY_OPTION '$buffer'" | \
-    cut -d ' ' -f 2- | sed -e 's/^/:\//' | string escape -n)
-  if test -n "$selected_files"
-    commandline "git add $selected_files"
-    commandline -f execute
-  end
-  commandline -f repaint
+    commandline | read -l buffer
+    set selected_files (git status --porcelain | angler "$ANGLER_QUERY_OPTION '$buffer'" | \
+      string sub -s 4 | sed -e 's/^/:\//' | string escape -n)
+    if test -n "$selected_files"
+        commandline "git add $selected_files"
+        commandline -f execute
+    end
+    commandline -f repaint
 end
