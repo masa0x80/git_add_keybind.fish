52a129727e4810f1a9cf51b2494d30e35aaf50c6
questbeat <questbeat@gmail.com>
2020-06-28 00:39:42 +0900

    Unescape selected filename

--- functions/__git_add.fish
+++ functions/__git_add.fish
@@ -1,7 +1,7 @@
 function __git_add
     commandline | read -l buffer
     set selected_files (git status --porcelain | angler "$ANGLER_QUERY_OPTION '$buffer'" | \
-      string sub -s 4 | sed -e 's/^/:\//' | string escape -n)
+      string sub -s 4 | sed -e 's/^/:\//' | string unescape | string escape -n)
     if test -n "$selected_files"
         commandline "git add $selected_files"
         commandline -f execute
